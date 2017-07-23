9d7f1be463ca0ba6007e84c30410569f6c5892ad
Dalton Turner <dalton.turner@west-mec.org>
2017-07-22 17:15:09 -0700

    Wrapped pdf-box methods in a PDF class

--- src/com/gammarush/main/Main.java
+++ src/com/gammarush/main/Main.java
@@ -1,37 +1,18 @@
 package com.gammarush.main;
 
-import java.io.File;
 import java.io.IOException;
-
-import org.apache.pdfbox.pdmodel.PDDocument;
-import org.apache.pdfbox.pdmodel.PDPage;
-import org.apache.pdfbox.pdmodel.PDPageContentStream;
-import org.apache.pdfbox.pdmodel.font.PDType1Font;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
-        File file = new File("res/input.pdf");
-        PDDocument document = PDDocument.load(file);
+        PDF pdf = new PDF("res/input.pdf");
     	
-    	int pageIndex = 0;
-    	for(PDPage page : document.getPages()) {
-    		
-			@SuppressWarnings("deprecation")
-			PDPageContentStream contentStream = new PDPageContentStream(document, page, true, true, true);
-    		contentStream.beginText();
-    		contentStream.setFont(PDType1Font.TIMES_ROMAN, 12);
-    		contentStream.newLineAtOffset(100, 100);
-    		contentStream.showText("Hello World");
-    		contentStream.endText();
-    		contentStream.close();
-    		
-            pageIndex++;
-        }
+        pdf.setFontSize(14);
+    	pdf.addText("The quick brown fox jumps over the lazy dog.", .25f, .5f, 0);
     	
     	System.out.println("PDF Saved");
-    	document.save("res/output.pdf");
-    	document.close();
+    	pdf.save("res/output.pdf");
+    	pdf.close();
     }
     
 }
---
+++ src/com/gammarush/main/PDF.java
@@ -0,0 +1,67 @@
+package com.gammarush.main;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.pdmodel.PDPage;
+import org.apache.pdfbox.pdmodel.PDPageContentStream;
+import org.apache.pdfbox.pdmodel.PDPageTree;
+import org.apache.pdfbox.pdmodel.encryption.InvalidPasswordException;
+import org.apache.pdfbox.pdmodel.font.PDFont;
+import org.apache.pdfbox.pdmodel.font.PDType1Font;
+
+public class PDF {
+	
+	private PDDocument document;
+	
+	private PDFont font = PDType1Font.HELVETICA;
+	private float fontSize = 12;
+	
+	public PDF(String path) throws InvalidPasswordException, IOException {
+		File file = new File(path);
+        document = PDDocument.load(file);
+	}
+	
+	public void addText(String text, float x, float y, PDPage page) throws IOException {
+		@SuppressWarnings("deprecation")
+		PDPageContentStream contentStream = new PDPageContentStream(document, page, true, true, true);
+		contentStream.beginText();
+		contentStream.setFont(font, fontSize);
+		contentStream.newLineAtOffset(x * page.getMediaBox().getWidth(), y * page.getMediaBox().getHeight());
+		contentStream.showText(text);
+		contentStream.endText();
+		contentStream.close();
+	}
+	
+	public void addText(String text, float x, float y, int pageIndex) throws IOException {
+		PDPage page = document.getPage(pageIndex);
+		addText(text, x, y, page);
+	}
+	
+	public PDPageTree getPages() {
+		return document.getPages();
+	}
+	
+	public void setFont(PDFont font, float fontSize) {
+		this.font = font;
+		this.fontSize = fontSize;
+	}
+	
+	public void setFont(PDFont font) {
+		setFont(font, this.fontSize);
+	}
+	
+	public void setFontSize(float fontSize) {
+		setFont(this.font, fontSize);
+	}
+	
+	public void save(String path) throws IOException {
+		document.save(path);
+	}
+	
+	public void close() throws IOException {
+		document.close();
+	}
+
+}
